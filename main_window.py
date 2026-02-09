6abe3c3f3101707ad5157766f9fdb8a999b187fc
Lucas <lucagabrielsantos5@gmail.com>
2026-02-09 07:54:32 -0300

    updt

---
+++ main_window.py
@@ -0,0 +1,20 @@
+from PySide6.QtWidgets import QMainWindow, QMessageBox, QVBoxLayout, QWidget
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
+        super().__init__(parent, *args, **kwargs)
+        self.cw = QWidget()
+        self.vLayout = QVBoxLayout()
+        self.cw.setLayout(self.vLayout)
+        self.setCentralWidget(self.cw)
+        self.setWindowTitle('Calculator')
+
+    def adjustFixedSize(self):
+        self.adjustSize()
+        self.setFixedSize(self.width(), self.height())
+
+    def addWidgetToVLayout(self, widget: QWidget):
+        self.vLayout.addWidget(widget)
+
+    def makeMsgBox(self):
+        return QMessageBox(self)
