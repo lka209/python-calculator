1b2323cadc442a7110e0fc1dc089b795c01d3d25
Lucas <lucagabrielsantos5@gmail.com>
2026-02-11 07:18:24 -0300

    updt

--- display.py
+++ display.py
@@ -3,8 +3,6 @@ from PySide6.QtWidgets import QLineEdit
 from PySide6.QtGui import QKeyEvent
 from variables import MINIMUM_WIDTH, TEXT_MARGIN, MEDIUM_FONT_SIZE
 from utils import isEmpty, isNumOrDot
-
-
 class Display(QLineEdit):
     eqPressed = Signal()
     delPressed = Signal()
@@ -25,69 +23,47 @@ class Display(QLineEdit):
         self.setTextMargins(*margins)
 
     def keyPressEvent(self, event: QKeyEvent) -> None:
-        text = event.text().strip()
+        text = event.text()
         key = event.key()
         KEYS = Qt.Key
 
-        isEnter = key in (
-            KEYS.Key_Enter,
-            KEYS.Key_Return,
-            KEYS.Key_Equal,
-        )
-
-        isDelete = key in (
-            KEYS.Key_Backspace,
-            KEYS.Key_Delete,
-        )
-
-        isEsc = key in (
-            KEYS.Key_Escape,
-            KEYS.Key_C,
-        )
-
-        isOperator = key in (
-            KEYS.Key_Plus,
-            KEYS.Key_Minus,
-            KEYS.Key_Slash,
-            KEYS.Key_Asterisk,
-            KEYS.Key_P,
-        )
-
-        # ENTER / =
-        if isEnter:
+        if key in (KEYS.Key_Enter, KEYS.Key_Return, KEYS.Key_Equal):
             self.eqPressed.emit()
             event.accept()
             return
 
-        # BACKSPACE / DELETE
-        if isDelete:
+        if key in (KEYS.Key_Backspace, KEYS.Key_Delete):
             self.delPressed.emit()
             event.accept()
             return
 
-        # ESC / C
-        if isEsc:
+        if key == KEYS.Key_Escape:
             self.clearPressed.emit()
             event.accept()
             return
-
-        # OPERADORES
-        if isOperator:
-            if text.lower() == "p":
-                text = "^"
+        
+        if key in (
+            KEYS.Key_Plus,
+            KEYS.Key_Minus,
+            KEYS.Key_Slash,
+            KEYS.Key_Asterisk,
+        ):
             self.operatorPressed.emit(text)
             event.accept()
             return
 
-        # IGNORA TECLAS SEM TEXTO
+        if key == KEYS.Key_P:
+            self.operatorPressed.emit("^")
+            event.accept()
+            return
+
         if isEmpty(text):
             event.ignore()
             return
 
-        # NÚMEROS E PONTO
         if isNumOrDot(text):
             self.inputPressed.emit(text)
             event.accept()
             return
-
+        
         super().keyPressEvent(event)
