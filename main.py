f7e5d88d8b041e7747745f7bd6d43accd1298cbe
Lucas <lucagabrielsantos5@gmail.com>
2026-02-13 08:44:51 -0300

    updt

--- main.py
+++ main.py
@@ -1,40 +1,33 @@
 import sys
-from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication
-from main_window import MainWindow
+
+from buttons import ButtonsGrid
 from display import Display
 from info import Info
-from buttons import ButtonsGrid
+from main_window import MainWindow
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication
+from styles import setupTheme
 from variables import WINDOW_ICON_PATH
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
+if __name__ == '__main__':
 
+    app = QApplication(sys.argv)
+    setupTheme()
     window = MainWindow()
-    window.setWindowTitle("Calculator")
 
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
 
-    display = Display()
-
+    info = Info('Sua conta')
     window.addWidgetToVLayout(info)
+
+    display = Display()
     window.addWidgetToVLayout(display)
 
-    window.buttonsGrid = ButtonsGrid(display, info)
-    window.vLayout.addLayout(window.buttonsGrid)
-
-    display.eqPressed.connect(lambda: window.buttonsGrid.handleKey("="))
-    display.delPressed.connect(lambda: window.buttonsGrid.handleKey("◀"))
-    display.clearPressed.connect(lambda: window.buttonsGrid.handleKey("C"))
-    display.inputPressed.connect(window.buttonsGrid.handleKey)
-    display.operatorPressed.connect(window.buttonsGrid.handleKey)
+    buttonsGrid = ButtonsGrid(display, info, window)
+    window.vLayout.addLayout(buttonsGrid)
 
     window.adjustFixedSize()
-    window.setFocus()
-    window.cw.setFocusPolicy(window.cw.StrongFocus)
-    window.cw.setFocus()
-
     window.show()
     app.exec()
